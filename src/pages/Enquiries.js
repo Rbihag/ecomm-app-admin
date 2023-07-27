d4a4af5d8cb7be02a45702143f11e9c200fcff78
Rbihag <bihag.rudolf@gmail.com>
2023-07-27 15:04:01 +0800

    start enquiry

---
+++ src/pages/Enquiries.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Enquiries = () => {
+    return (
+        <div>
+            <h3 className="mb-4 title">Enquiries</h3>
+            <div>
+                <Table columns={columns} dataSource={data1} />
+            </div>
+        </div>
+    )
+}
+
+export default Enquiries

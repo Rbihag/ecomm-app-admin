8850c2da15b3b573cc1ce5d566085e76418c36a7
Rbihag <bihag.rudolf@gmail.com>
2023-07-27 11:39:53 +0800

    start with admin dashboard

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CustomInput from '../components/CustomInput.js'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
     return (
@@ -17,21 +18,19 @@ const Login = () => {
                         type="text"
                         label="Email Address"
                         id="email"
-                        name="email"
                     />
                     <CustomInput
                         type="password"
                         label="Password"
                         id="pass"
-                        name="password"
                     />
-                    <button
+                    <Link to='/admin'
                         className="border-0 px-3 py-2 text-white fw-bold w-100 text-center text-decoration-none fs-5"
                         style={{ background: "#ffd333" }}
                         type="submit"
                     >
                         Login
-                    </button>
+                    </Link>
                 </form>
             </div>
         </div>

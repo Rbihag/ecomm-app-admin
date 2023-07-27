66cda55759fb930ebde9461c050fc94509ed83cb
Rbihag <bihag.rudolf@gmail.com>
2023-07-27 13:01:17 +0800

    start header admin app

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -2,12 +2,20 @@ import React, { useState } from 'react';
 import {
     MenuFoldOutlined,
     MenuUnfoldOutlined,
-    UploadOutlined,
-    UserOutlined,
-    VideoCameraOutlined,
 } from '@ant-design/icons';
+import {
+    AiOutlineDashboard,
+    AiOutlineShoppingCart,
+    AiOutlineUser,
+    AiOutlineBgColors,
+} from 'react-icons/ai';
+import { RiCouponLine } from "react-icons/ri";
+import { ImBlog } from "react-icons/im";
+import { FaClipboardList } from "react-icons/fa";
+import { SiBrandfolder } from "react-icons/si";
+import { BiCategoryAlt } from "react-icons/bi";
 import { Layout, Menu, Button, theme } from 'antd';
-
+import { useNavigate } from 'react-router-dom';
 const { Header, Sider, Content } = Layout;
 
 const MainLayout = () => {
@@ -16,36 +24,122 @@ const MainLayout = () => {
         token: { colorBgContainer },
     } = theme.useToken();
 
+    const navigate = useNavigate();
+
     return (
         <Layout>
             <Sider trigger={null} collapsible collapsed={collapsed}>
-                <div className='logo' />
-                <div className="demo-logo-vertical" />
+                <div className="logo">
+                    <h2 className="text-white fs-5 text-center py-3 mb-0">
+                        <span className="sm-logo">AUA</span>
+                        <span className="lg-logo">Ara Ukay Alaminos</span>
+                    </h2>
+                </div>
                 <Menu
                     theme="dark"
                     mode="inline"
-                    defaultSelectedKeys={['1']}
+                    defaultSelectedKeys={['']}
+                    onClick={({ key }) => {
+                        if (key === "signout") {
+                        } else {
+                            navigate(key);
+                        }
+                    }}
                     items={[
                         {
-                            key: '1',
-                            icon: <UserOutlined />,
-                            label: 'nav 1',
+                            key: "",
+                            icon: <AiOutlineDashboard className="fs-4" />,
+                            label: "Dashboard",
                         },
                         {
-                            key: '2',
-                            icon: <VideoCameraOutlined />,
-                            label: 'nav 2',
+                            key: "customers",
+                            icon: <AiOutlineUser className="fs-4" />,
+                            label: "Customers",
                         },
                         {
-                            key: '3',
-                            icon: <UploadOutlined />,
-                            label: 'nav 3',
+                            key: "Catalog",
+                            icon: <AiOutlineShoppingCart className="fs-4" />,
+                            label: "Catalog",
+                            children: [
+                                {
+                                    key: "product",
+                                    icon: <AiOutlineShoppingCart className="fs-4" />,
+                                    label: "Add Product",
+                                },
+                                {
+                                    key: "list-product",
+                                    icon: <AiOutlineShoppingCart className="fs-4" />,
+                                    label: "Product List",
+                                },
+                                {
+                                    key: "brand",
+                                    icon: <SiBrandfolder className="fs-4" />,
+                                    label: "Brand",
+                                },
+                                {
+                                    key: "list-brand",
+                                    icon: <SiBrandfolder className="fs-4" />,
+                                    label: "Brand List ",
+                                },
+                                {
+                                    key: "category",
+                                    icon: <BiCategoryAlt className="fs-4" />,
+                                    label: "Category",
+                                },
+                                {
+                                    key: "list-category",
+                                    icon: <BiCategoryAlt className="fs-4" />,
+                                    label: "Category List",
+                                },
+                                {
+                                    key: "color",
+                                    icon: <AiOutlineBgColors className="fs-4" />,
+                                    label: "Color",
+                                },
+                                {
+                                    key: "list-color",
+                                    icon: <AiOutlineBgColors className="fs-4" />,
+                                    label: "Color List",
+                                },
+                            ],
+                        },
+                        {
+                            key: "orders",
+                            icon: <FaClipboardList className="fs-4" />,
+                            label: "Orders",
+                        },
+                        {
+                            key: "marketing",
+                            icon: <RiCouponLine className="fs-4" />,
+                            label: "Marketing",
+                            children: [
+                                {
+                                    key: "coupon",
+                                    icon: <ImBlog className="fs-4" />,
+                                    label: "Add Coupon",
+                                },
+                                {
+                                    key: "coupon-list",
+                                    icon: <RiCouponLine className="fs-4" />,
+                                    label: "Coupon List",
+                                },
+                            ],
+                        },
+                        {
+                            key: "enquiries",
+                            icon: <FaClipboardList className="fs-4" />,
+                            label: "Enquiries",
                         },
                     ]}
                 />
             </Sider>
-            <Layout>
-                <Header style={{ padding: 0 }}>
+            <Layout className='site-layout'>
+                <Header className="d-flex justify-content-between ps-1 pe-5"
+                    style={{
+                        padding: 0,
+                        background: colorBgContainer,
+                    }}
+                >
                     <Button
                         type="text"
                         icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
@@ -67,7 +161,7 @@ const MainLayout = () => {
                     Content
                 </Content>
             </Layout>
-        </Layout>
+        </Layout >
     );
 };
 

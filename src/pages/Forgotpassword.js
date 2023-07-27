369b88c3f3df900f3ea8f7d82281510c7a8c735e
Rbihag <bihag.rudolf@gmail.com>
2023-07-27 11:36:48 +0800

    finish forgot and reset password

--- src/pages/Forgotpassword.js
+++ src/pages/Forgotpassword.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import CustomInput from '../components/CustomInput.js'
 
 const Forgotpassword = () => {
     return (
-        <div>Forgotpassword</div>
+        <div className="py-5" style={{ background: "#ffd333", minHeight: "100vh" }}>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <div className="my-5 w-25 bg-white rounded-3 mx-auto p-4">
+                <h3 className="text-center title">Forgot Password</h3>
+                <p className="text-center">Please Enter Your Registered Email</p>
+                <form action=''>
+                    <CustomInput
+                        type="password"
+                        label="Email Address"
+                        id="email"
+                    />
+
+                    <button
+                        className="border-0 px-3 py-2 text-white fw-bold w-100"
+                        style={{ background: "#ffd333" }}
+                        type="submit"
+                    >
+                        Send Link
+                    </button>
+                </form>
+            </div>
+        </div>
     )
 }
 

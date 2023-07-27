5166dd493bf0faed2e15859cbab8d71681fb94ed
Rbihag <bihag.rudolf@gmail.com>
2023-07-27 13:14:13 +0800

    dashboard header

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -9,12 +9,15 @@ import {
     AiOutlineUser,
     AiOutlineBgColors,
 } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import { IoIosNotifications } from 'react-icons/io';
 import { RiCouponLine } from "react-icons/ri";
 import { ImBlog } from "react-icons/im";
 import { FaClipboardList } from "react-icons/fa";
 import { SiBrandfolder } from "react-icons/si";
 import { BiCategoryAlt } from "react-icons/bi";
-import { Layout, Menu, Button, theme } from 'antd';
+import { Layout, Menu, theme } from 'antd';
 import { useNavigate } from 'react-router-dom';
 const { Header, Sider, Content } = Layout;
 
@@ -134,22 +137,68 @@ const MainLayout = () => {
                 />
             </Sider>
             <Layout className='site-layout'>
-                <Header className="d-flex justify-content-between ps-1 pe-5"
+                <Header
+                    className="d-flex justify-content-between ps-1 pe-5"
                     style={{
                         padding: 0,
                         background: colorBgContainer,
                     }}
                 >
-                    <Button
-                        type="text"
-                        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-                        onClick={() => setCollapsed(!collapsed)}
-                        style={{
-                            fontSize: '16px',
-                            width: 64,
-                            height: 64,
-                        }}
-                    />
+                    {React.createElement(
+                        collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+                        {
+                            className: "trigger",
+                            onClick: () => setCollapsed(!collapsed),
+                        }
+                    )}
+                    <div className="d-flex gap-4 align-items-center">
+                        <div className="position-relative">
+                            <IoIosNotifications className="fs-4" />
+                            <span className="badge bg-warning rounded-circle p-1 position-absolute">
+                                3
+                            </span>
+                        </div>
+
+                        <div className="d-flex gap-3 align-items-center dropdown">
+                            <div>
+                                <img
+                                    width={32}
+                                    height={32}
+                                    src="https://res.cloudinary.com/dycuomdir/image/upload/v1690341474/IMG_20220805_132429.jpg_ljurth.png"
+                                    alt=""
+                                />
+                            </div>
+                            <div
+                                role="button"
+                                id="dropdownMenuLink"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                <h5 className="mb-0">Rbihag</h5>
+                                <p className="mb-0">rbihag@icloud.com</p>
+                            </div>
+                            <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                <li>
+                                    <Link
+                                        className="dropdown-item py-1 mb-1"
+                                        style={{ height: "auto", lineHeight: "20px" }}
+                                        to="/"
+                                    >
+                                        View Profile
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        className="dropdown-item py-1 mb-1"
+                                        style={{ height: "auto", lineHeight: "20px" }}
+                                        to="/"
+                                    >
+                                        Signout
+                                    </Link>
+                                </li>
+                            </div>
+                        </div>
+                    </div>
                 </Header>
                 <Content
                     style={{
@@ -158,7 +207,7 @@ const MainLayout = () => {
                         minHeight: 280
                     }}
                 >
-                    Content
+                    <Outlet />
                 </Content>
             </Layout>
         </Layout >
